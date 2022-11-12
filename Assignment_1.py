18ac32444b6d67648e4205feb9c12cd7235c26da
Byomkesh07 <117578118+Byomkesh07@users.noreply.github.com>
2022-11-12 16:22:05 +0530

    Update and rename Assignment_1 to Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,9 +1,9 @@
-16. Write a code that gives following as an output.
-    iNeuroniNeuroniNeuroniNeuron
-Ans. 
+#16. Write a code that gives following as an output.
+    #iNeuroniNeuroniNeuroniNeuron
+    
     print("iNeuron"*4)
 
-17. Write a code to take a number as an input from the user and check if the number is odd or even.
+#17. Write a code to take a number as an input from the user and check if the number is odd or even.
 
 #Decalaring Variables
 
@@ -13,7 +13,7 @@ if (a%2==0):
 else:
     print("The No. ",a," is an odd Number")   
 
-22. Write a code to take the age of person as an input and if age >= 18 display "I can vote". If age is < 18 display "I can't vote".
+#22. Write a code to take the age of person as an input and if age >= 18 display "I can vote". If age is < 18 display "I can't vote".
 
 #Declaring Variables
 
@@ -23,7 +23,7 @@ if (age>=18):
 else:
     print("I can't vote")
 
-23. Write a code that displays the sum of all the even numbers from the given list.
+#23. Write a code that displays the sum of all the even numbers from the given list.
 
 num = [12, 75, 150, 180, 145, 525, 50]
 sum=0
@@ -32,7 +32,7 @@ for i in num:
     sum=sum+i
 print("The sum of the given even numbers is ",sum)
 
-24. Write a code to take 3 numbers as an input from the user and display the greatest no as output.
+#24. Write a code to take 3 numbers as an input from the user and display the greatest no as output.
 
 a = int(input("Enter first no.: "))
 b = int(input("Enter second no.: "))
@@ -46,8 +46,8 @@ elif(c>a and c>b):
 else:
     print("All are equal")
 
-25. Write a program to display only those numbers from a list that satisfy the following conditions
-- The number must be divisible by five.
+#25. Write a program to display only those numbers from a list that satisfy the following conditions
+#- The number must be divisible by five.
 
 a=[10,24,30,45,51,60,65]
 for i in a:
