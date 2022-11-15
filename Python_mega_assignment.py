a974d13645553e315cd7e13b019719070442e633
Byomkesh07 <117578118+Byomkesh07@users.noreply.github.com>
2022-11-16 00:58:56 +0530

    Update Python_mega_assignment.py

--- Python_mega_assignment.py
+++ Python_mega_assignment.py
@@ -5,3 +5,12 @@
 
 str1 = "Big Data iNeuron"
 print(str1[9 : 16])
+
+# Q29. Write a code to get the desired output of the following
+
+# string = "Big Data iNeuron"
+# desired_output = "norueNi"
+
+str1 = "Big Data iNeuron"
+for i in range((len(str1)-1),8,-1):
+    print(str1[i],end="")
